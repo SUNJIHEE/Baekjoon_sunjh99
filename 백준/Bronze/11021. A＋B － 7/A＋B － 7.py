3f5de92d0a13eb72a95f34d3d9e8ad1cdf53a3a0
SUNJIHEE <106293333+SUNJIHEE@users.noreply.github.com>
2023-09-04 00:27:54 +0900

    [Bronze V] Title: A+B - 7, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.py
@@ -0,0 +1,9 @@
+import sys
+N = int(input())
+a = []
+for _ in range(N):
+    n, m = map(int, sys.stdin.readline().split())
+    a.append(n+m)
+
+for i in range(N):
+    print('Case #',i+1,': ',a[i], sep='')
